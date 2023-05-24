ae90943fec38f58ab72e901f4e1aca7b5950afdb
Őze Laura <ozelara@gmail.com>
2023-05-24 10:35:00 +0200

    Első 2 feladat

--- Operatorok/Program.cs
+++ Operatorok/Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 
 //1.Feladat
+List<string> kifejezesek = new List<string>();
+foreach (var item in File.ReadAllLines("kifejezesek.txt"))
+{
+    kifejezesek.Add(item);
+}
+
+//2.feladat
+Console.WriteLine($"2.feladat: Kifejezések száma: {kifejezesek.Count}");
