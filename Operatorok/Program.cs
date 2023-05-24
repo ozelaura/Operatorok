4b82390939e707f1a96be679b6b6fe340dc67fa1
Őze Laura <ozelara@gmail.com>
2023-05-24 10:54:43 +0200

    Kész a 4. feladatig

--- Operatorok/Program.cs
+++ Operatorok/Program.cs
@@ -9,4 +9,32 @@ foreach (var item in File.ReadAllLines("kifejezesek.txt"))
 }
 
 //2.feladat
-Console.WriteLine($"2.feladat: Kifejezések száma: {kifejezesek.Count}");
+Console.WriteLine($"2. feladat: Kifejezések száma: {kifejezesek.Count}");
+
+//3.feladat
+Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {kifejezesek.Where(x => x.Contains("mod")).Count()}");
+
+//4.feladat
+bool van = false;
+foreach (var item in kifejezesek)
+{
+    string[] itemElemek = item.Split(' ');
+    var elsoSzam = int.Parse(itemElemek[0]);
+    var masodikSzam = int.Parse(itemElemek[2]);
+
+    if (elsoSzam % 10 == 0 && masodikSzam % 10 == 0)
+    {
+        van = true;
+        break;
+    }
+}
+
+if (van)
+{
+    Console.WriteLine("4. feladat: Van ilyen kifejezés!");
+}
+else
+{
+    Console.WriteLine("4. feladat: Nincs ilyen kifejezés!");
+
+}
